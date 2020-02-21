e3d9375e4aa57efdf4862830a358d6af885760fe
riccardou <christianr@rocketmail.com>
2020-02-21 14:59:30 -0800

    add initialized populateQuestionList function

--- js/controllers/quizit-controller.js
+++ js/controllers/quizit-controller.js
@@ -4,6 +4,7 @@
         init: function() {
             app.quizItModel.init();
             app.quizItView.init();
+            app.quizItView.populateQuestionList(app.quizItModel.quizList);
             console.log('controller initialised');
         }
     };
