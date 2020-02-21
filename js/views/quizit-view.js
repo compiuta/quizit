35494a5a7b8309618e689564367d765822a960d6
riccardou <christianr@rocketmail.com>
2020-02-21 14:57:52 -0800

    add populateListItem and populateQuestionList to populate quiz list container

--- js/views/quizit-view.js
+++ js/views/quizit-view.js
@@ -1,14 +1,33 @@
 (function(window) {
     'use strict';
     let quizItView = {
+        populateListItem: function(data) {
+            let quizListItemTemplateClone = this.quizListItemTemplate.content.cloneNode(true);
+            quizListItemTemplateClone.querySelector('[data-js="quizListItemTitle"]').innerText = data.quizName;
+            quizListItemTemplateClone.querySelector('[data-js="quizListItemCounter"]').innerText = data.quizTakenCount;
+            return quizListItemTemplateClone;
+        },
+        populateQuestionList: function(data) {
+            let fragment = document.createDocumentFragment();
+
+            for(let key in data) {
+                fragment.appendChild(this.populateListItem(data[key]));
+            }
+
+            this.quizListContainer.appendChild(fragment);
+
+        },
         getDomElements: function() {
             this.quizListContainer = document.querySelector('[data-js="quizListContainer"]');
             this.quizModal = document.querySelector('[data-js="quizModal"]');
             this.quizTitle = document.querySelector('[data-js="quizTitle"]');
             this.quizQuestionsContainer = document.querySelector('[data-js="quizQuestionsContainer"]');
+            this.quizListItemTemplate = document.querySelector('[data-js="quizListItemTemplate"]');
         },
         init: function() {
             this.getDomElements();
+            console.log('view initialised');
+            
         }
     };
 
