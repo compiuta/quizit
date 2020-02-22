033662bae5f791baf2807d7a174ba1f3610eba86
riccardou <christianr@rocketmail.com>
2020-02-22 12:24:33 -0800

    add startQuiz functionality

--- js/controllers/quizit-controller.js
+++ js/controllers/quizit-controller.js
@@ -1,6 +1,12 @@
 (function(window) {
     'use strict';
     let quizItController = {
+        startQuiz: function() {
+            let dataCell = this.dataset.js;
+            let selectedCellData = app.quizItModel.getData(dataCell);
+            app.quizItView.populateQuizModal(selectedCellData);
+            app.quizItView.toggleQuizModal();
+        },
         init: function() {
             app.quizItModel.init();
             app.quizItView.init();
