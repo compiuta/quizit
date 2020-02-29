4c7811d82faa00b6ca77d494428738d77706b06f
compiuta <christianr@rocketmail.com>
2020-02-29 09:12:13 -0800

    add checkAnswer functionality for selected choice in quiz

--- js/controllers/quizit-controller.js
+++ js/controllers/quizit-controller.js
@@ -3,8 +3,8 @@
     let quizItController = {
         startQuiz: function() {
             app.quizItView.toggleLoader();
-            let quizID = this.dataset.js;
-            app.quizItModel.getData(quizID);
+            app.quizItModel.currentQuizID = this.dataset.js;
+            app.quizItModel.getData(app.quizItModel.currentQuizID);
             app.quizItView.toggleQuizModal();
         },
         populateListDataWhenReady: function() {
@@ -15,6 +15,19 @@
             app.quizItView.populateQuizModal(app.quizItModel.quizList[quizID], app.quizItModel.selectedQuizData[quizID].quizQuestions);
             
             setTimeout(app.quizItView.toggleLoader, 1000);
+        },
+        checkAnswer: function(e) {
+            e.preventDefault();
+            app.quizItView.quizInputChoiceElements.forEach(element => {
+                
+                if(element.checked) {
+                    if(element.value === app.quizItModel.selectedQuizData[app.quizItModel.currentQuizID].quizQuestions[app.quizItView.currentQuestionCounter].answer) {
+                        alert('correct')
+                    } else {
+                        alert('wrong');
+                    }
+                }
+            });
         },
         init: function() {
             app.quizItModel.init();
