3f7fd1ac776b7e8610541b355110f827fda8f230
compiuta <christianr@rocketmail.com>
2020-03-01 12:29:34 -0800

    add toggle buttons, show results and toggle alert functionality

--- js/views/quizit-view.js
+++ js/views/quizit-view.js
@@ -42,23 +42,49 @@
         },
         populateQuizModal: function(quizInfo, quizData) {
 
-            if(app.quizItView.currentQuestionCounter > Object.keys(quizData).length) {
-                app.quizItView.showQuizResults();
-                return;
-            }
-
             console.log(quizData);
             app.quizItView.quizTitle.innerText = quizInfo.quizName;
             app.quizItView.populateModalQuestionElements(quizData[app.quizItView.currentQuestionCounter]);
-            app.quizItView.currentQuestionCounter++
         },
-        showQuizResults: function() {
+        showQuizResults: function(score) {
+            app.quizItView.quizQuestionsContainer.innerHTML = 'end of quiz. your score is ' + score;
             app.quizItView.currentQuestionCounter = 1;
-
+            app.quizItView.toggleAnswerMessage(true, 0, true);
+            app.quizItView.toggleLoader();
         },
         toggleQuizModal: function() {
             app.quizItView.quizModal.classList.toggle('hide-quiz');
             app.quizItView.quizModal.classList.toggle('reveal-quiz');
+        },
+        toggleAnswerMessage: function(isCorrect, answer, nextQuestion) {
+
+            if(nextQuestion) {
+                app.quizItView.answerAlert.classList.add('hide');
+                app.quizItView.answerAlert.classList.remove('alert-success');
+                app.quizItView.answerAlert.classList.remove('alert-warning');
+                return;
+            }
+
+            if(isCorrect) {
+                app.quizItView.answerMessage.innerText = 'Correct!'
+                app.quizItView.answerAlert.classList.add('alert-success');
+            } else {
+                app.quizItView.answerMessage.innerText = 'Wrong! The correct answer is: ' + answer;
+                app.quizItView.answerAlert.classList.add('alert-warning');
+            }
+
+            app.quizItView.answerAlert.classList.remove('hide');
+        },
+        toggleQuizButtons: function() {
+            app.quizItView.submitAnswerButton.classList.toggle('hide');
+            app.quizItView.nextQuestionButton.classList.toggle('hide');
+        },
+        resetRadioButtons: function() {
+            app.quizItView.quizInputChoiceElements.forEach(element => {
+                if(element.checked) {
+                    element.checked = false;
+                }
+            });
         },
         getDomElements: function() {
             this.loader = document.querySelector('[data-js="loader"]');
@@ -75,10 +101,13 @@
             this.submitAnswerButton = document.querySelector('[data-js="submitAnswer"]');
             this.nextQuestionButton = document.querySelector('[data-js="nextQuestion"]');
             this.quizInputChoiceElements = document.querySelectorAll('[name="choice"]');
+            this.answerAlert = document.querySelector('[js-data="answerAlert"]');
+            this.answerMessage = document.querySelector('[js-data="answerMessage"]');
         },
         addEventListeners: function() {
             this.quizModalClose.addEventListener('click', this.toggleQuizModal);
             this.submitAnswerButton.addEventListener('click', app.quizItController.checkAnswer);
+            this.nextQuestionButton.addEventListener('click', app.quizItController.nextQuestion);
         },
         init: function() {
             this.getDomElements();
